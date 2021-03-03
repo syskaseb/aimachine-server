db4edeb4deb0e15c06470477141b7d2fde335a86
Shug <tomaszlesiak0@gmail.com>
2021-03-03 18:17:04 +0100

    initialize spring boot

---
+++ src/test/kotlin/ai/aimachineserver/AimachineServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package ai.aimachineserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AimachineServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
